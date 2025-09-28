2af969d69c3a0f9422d0095b90efc64d71781caa
rdvn-ylmz <ridvanyilmaz333@gmail.com>
2025-09-29 00:40:47 +0300

    learning phase

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def get_num_words(path_to_file):
-    with open(path_to_file,"r") as book:
-        word_bank = book.read().split()
-        print(len(word_bank))
+from stats import get_num_words
 
 
 
-
-
-
+get_num_words()
--- stats.py
+++ stats.py
@@ -1,2 +1,6 @@
-from stats import get_num_words
-get_book_words()
+def get_num_words(path_to_file):
+    with open(path_to_file,"r") as book:
+        word_bank = book.read().split()
+        print(len(word_bank))
+
+
